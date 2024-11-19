b2102349ce15ed1373c6f27f7d32e418f0517cc2
David <davidjim83@icloud.com>
2024-11-20 00:42:31 +0100

    Initial Commit

---
+++ Life Days/Life_DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_DaysApp.swift
+//  Life Days
+//
+//  Created by David Jiménez on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Life_DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Life DaysTests/Life_DaysTests.swift
@@ -0,0 +1,16 @@
+//
+//  Life_DaysTests.swift
+//  Life DaysTests
+//
+//  Created by David Jiménez on 20/11/24.
+//
+
+import Testing
+
+struct Life_DaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
